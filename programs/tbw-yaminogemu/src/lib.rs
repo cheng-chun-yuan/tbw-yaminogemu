6703a5b2908f91ae0f73d38dbebf45534ac0d1e3
cheng-chun-yuan <albert2367593@gmail.com>
2024-12-02 00:13:10 +0800

    feat: escrow template

---
+++ programs/tbw-yaminogemu/src/lib.rs
@@ -0,0 +1,31 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("5SoL48UjfnBTkAHzi7xvuizLErjiUsJKcXfwCABThfgd");
+
+#[program]
+pub mod tbw_yaminogemu {
+    use super::*;
+
+    pub fn make(ctx: Context<Make>, seed: u64, deposit: u64, receive: u64) -> Result<()> {
+        ctx.accounts.deposit(deposit)?;
+        ctx.accounts.save_escrow(seed, receive, &ctx.bumps)
+    }
+
+    pub fn refund(ctx: Context<Refund>) -> Result<()> {
+        ctx.accounts.refund_and_close_vault()
+    }
+
+    pub fn take(ctx: Context<Take>) -> Result<()> {
+        ctx.accounts.deposit()?;
+        ctx.accounts.withdraw_and_close_vault()
+    }
+}
